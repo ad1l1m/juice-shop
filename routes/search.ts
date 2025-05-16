80bac19775f471f6172fedc1c0dcd3cf1b8204fe
ad1l <adil.alimbaev.06@gmail.com>
2025-05-16 14:57:13 +0500

    fix

--- routes/search.ts
+++ routes/search.ts
@@ -9,6 +9,7 @@ import { UserModel } from '../models/user'
 import { challenges } from '../data/datacache'
 import { JSDOM } from 'jsdom'
 import createDOMPurify from 'dompurify'
+import { Op, QueryTypes } from 'sequelize'
 
 import * as utils from '../lib/utils'
 const challengeUtils = require('../lib/challengeUtils')
@@ -21,75 +22,40 @@ const DOMPurify = createDOMPurify(window)
 
 // vuln-code-snippet start unionSqlInjectionChallenge dbSchemaChallenge
 module.exports = function searchProducts () {
-  console.log('////////////////////')
-  // console.log('hello from search file')
-  return (req: Request, res: Response, next: NextFunction) => {
-    // console.log('Query params:', req)
-    // console.log('Response params', res)
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    // console.log('Raw q:', req.query.q)
-    // console.log('Processed criteria:', criteria) 
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    // console.log(req.query.q)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
-      .then(([products]: any) => {
-      //   for (let i = 0; i < products.length; i++) {
-      //     products[i].name = req.__(DOMPurify.sanitize(products[i].name))
-      //     products[i].description = req.__(DOMPurify.sanitize(products[i].description))
-      // }             
-        const dataString = JSON.stringify(products)
-        // console.log(dataString, 'dataString')
-        if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
-          let solved = true
-          UserModel.findAll().then(data => {
-            const users = utils.queryResultToJson(data)
-            if (users.data?.length) {
-              for (let i = 0; i < users.data.length; i++) {
-                solved = solved && utils.containsOrEscaped(dataString, users.data[i].email) && utils.contains(dataString, users.data[i].password)
-                if (!solved) {
-                  break
-                }
-              }
-              if (solved) {
-                challengeUtils.solve(challenges.unionSqlInjectionChallenge)
-              }
-            }
-          }).catch((error: Error) => {
-            next(error)
-          })
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      /* 1. Строка поиска, максимум 200 симв. */
+      const criteria = String(req.query.q ?? '').slice(0, 200)
+
+      /* 2. Параметризованный raw-query */
+      const [products] = await models.sequelize.query(
+        `SELECT *
+           FROM Products
+          WHERE ((name ILIKE :search OR description ILIKE :search)
+             AND deletedAt IS NULL)
+          ORDER BY name`,
+        {
+          replacements: { search: `%${criteria}%` },
+          type: QueryTypes.SELECT
         }
-        if (challengeUtils.notSolved(challenges.dbSchemaChallenge)) {
-          let solved = true
-          void models.sequelize.query('SELECT sql FROM sqlite_master').then(([data]: any) => {
-            const tableDefinitions = utils.queryResultToJson(data)
-            // console.log(tableDefinitions, 'tableDefinition')
-            if (tableDefinitions.data?.length) {
-              for (let i = 0; i < tableDefinitions.data.length; i++) {
-                if (tableDefinitions.data[i].sql) {
-                  solved = solved && utils.containsOrEscaped(dataString, tableDefinitions.data[i].sql)
-                  if (!solved) {
-                    break
-                  }
-                }
-              }
-              if (solved) {
-                challengeUtils.solve(challenges.dbSchemaChallenge)
-              }
-            }
-          })
-        } // vuln-code-snippet hide-end
-        for (let i = 0; i < products.length; i++) {
-          // console.log(products,'products')
-          products[i].name = req.__(DOMPurify.sanitize(products[i].name));
-          products[i].description = req.__(DOMPurify.sanitize(products[i].description));
-          // products[i].name = req.__(products[i].name)
-          // products[i].description = req.__(products[i].description)
-        }
-        res.json(utils.queryResultToJson(products))
-        // console.log(products+'   products')
-      }).catch((error: ErrorWithParent) => {
-        next(error.parent)
-      })
+      )
+
+      /* 3. Оставляем логику челленджей (если нужна) */
+      // const dataString = JSON.stringify(products) ...
+      // challengeUtils.solveIf(...)
+
+      /* 4. XSS-гигиена + локализация */
+      for (const p of products as any[]) {
+        p.name        = req.__(DOMPurify.sanitize(p.name))
+        p.description = req.__(DOMPurify.sanitize(p.description))
+      }
+
+      res.json(utils.queryResultToJson(products))
+    } catch (err) {
+      /* Перехватываем ошибку SQL/Sequelize */
+      next(err)
+    }
   }
 }
+
 // vuln-code-snippet end unionSqlInjectionChallenge dbSchemaChallenge
